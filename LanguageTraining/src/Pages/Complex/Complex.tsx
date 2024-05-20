c1165fffc99c48e5dba7c6bf50a0216c55a713c3
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-05-20 15:52:37 -0300

    Update Complex Search
    Agora ele pesquisa e toca o audio, alem disso, tem botoes que ocultam e mostram a resposta

--- LanguageTraining/src/Pages/Complex/Complex.tsx
+++ LanguageTraining/src/Pages/Complex/Complex.tsx
@@ -3,31 +3,79 @@ import { db } from "../../App";
 import { useState } from "react";
  
 export const Complex = () => {
-    const [phrase, setPhrase] = useState('');
+    const [portuguesePhrase, setPortuguesePhrase] = useState(''); 
+    const [englishPhrase, setEnglishPhrase] = useState('');
+    const [hidePortuguese, setHidePortuguese] = useState(true);
+    const [hideEnglish, setHideEnglish] = useState(true);
+
     const dbRef = ref(db);
     
-    const search = () =>{
+    const getRandomPhrase = () => {
         const id = Math.floor(Math.random() * 50);
-    
-        get(child(dbRef, `Complex/${id}` )).then((snapshot) => {
+
+        get(child(dbRef, `Complex/${id}`)).then((snapshot) => {
             if (snapshot.exists()) {
-              console.log(snapshot.val());
-                setPhrase(snapshot.val().phrase);
-            } else {
-              console.log("No data available");
+                console.log(snapshot.val());
+                setPortuguesePhrase(snapshot.val().portuguese_translation);
+                setEnglishPhrase(snapshot.val().english_phrase); 
+                console.log("No data available");
             }
-          }).catch((error) => {
+        }).catch((error) => {
             console.error(error);
-          });
+        });
     }
+
+
+    const phraseToSpeech = () => {
+        const speech = new SpeechSynthesisUtterance();
+        speech.lang = 'en-US';
+        speech.text = englishPhrase;
+        speech.rate = 0.6;
+        window.speechSynthesis.speak(speech);
+    }
+    
+    
     
     return ( <>
         <h1>Complex</h1>
-        <p>Click in this button below to generate a random phrase in English: 
-            <input type="submit" onClick={search}/>
+        <p>Click in this button below to generate a random phrase in English:  
+            <input type="submit" onClick={getRandomPhrase}/>
         </p>
         
-        <textarea rows={4} cols={50} placeholder="Type what you are hearing..."></textarea>
+        <textarea rows={4} cols={50} placeholder="Type what you are hearing..."></textarea><br/>
+        <input type="submit" value="Play" onClick={phraseToSpeech}/>
+        
+        <br/>
+        <p>
+            <label>Portuguese phrase: </label>
+            {!hidePortuguese && <p>{portuguesePhrase}</p>}
+            <input
+                type="submit"
+                value="Show"
+                onClick={() => {
+                    if (!hidePortuguese) {
+                        setHidePortuguese(true);
+                    } else {
+                        setHidePortuguese(false);
+                    }
+                }}
+            />
+        </p>
+        <p>
+            <label>English phrase: </label>
+            {!hideEnglish && <p>{englishPhrase}</p>}
+            <input
+                type="submit"
+                value="Show"
+                onClick={() => {
+                    if (!hideEnglish) {
+                        setHideEnglish(true);
+                    } else {
+                        setHideEnglish(false);
+                    }
+                }}
+            />
+        </p>
     </>);
 }
     
