d4f0b62d68b816dfb6de131ca2735ba37ea3608c
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-05-20 15:20:09 -0300

    Routes

--- LanguageTraining/src/App.tsx
+++ LanguageTraining/src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './Pages/Home/Home'
+import { About } from './Pages/About/About'
+import { Complex } from './Pages/Complex/Complex'
+
+import { initializeApp } from "firebase/app"
+import { getDatabase } from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBpApVa6e-M51F_EmdC4eAPNq43qr6TO8",
+  authDomain: "languagetraining-ec4c0.firebaseapp.com",
+  databaseURL: "https://languagetraining-ec4c0-default-rtdb.firebaseio.com",
+  projectId: "languagetraining-ec4c0",
+  storageBucket: "languagetraining-ec4c0.appspot.com",
+  messagingSenderId: "429891473314",
+  appId: "1:429891473314:web:28bd9aad82c65453936cfb",
+  measurementId: "G-5CRN2DBK2T"
+};
+
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getDatabase(app);  
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/complex" element={<Complex />} />
+      </Routes>
+    </Router>
     </>
   )
 }
