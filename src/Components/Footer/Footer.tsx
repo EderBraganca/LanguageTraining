c7fba2e7f5d79211d38be15606d2d348e3981ead
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2025-01-12 20:53:19 -0300

    Upgrade footer link

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
     return (
         <footer className="footer">
             <section className="footerSection">
-                <a href="/about">About</a>
+                <a href="https://ederbraganca.github.io/" target="_blank" rel="noreferrer">About me</a>
                 <a href="/" onClick={alertEmail}>Do you need help? Send me an email</a>
             </section>
         </footer>
